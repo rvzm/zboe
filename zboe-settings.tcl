0c8fc757c60c29799ddf2c9462accabf13c68d81
rvzm <rvzm@protonmail.com>
2023-08-17 06:32:08 -0800

    removed unnecessary tags and setting

--- zboe-settings.tcl
+++ zboe-settings.tcl
@@ -7,11 +7,9 @@ namespace eval zboe {
 		namespace eval gen {
 			variable pubtrig "@"
 			variable controller "~z"
-			variable nick "zboe"
 			variable homechan "#fuknz"
 		}
 		namespace eval hunt {
-			# hunt trigger, higher means more often, max of 15
 			variable trigger "15"
 			variable accuracy "7"
 			variable time "5"
