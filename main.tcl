7b44a2f0d5e6031d19f3ea38fa14ec65da35aabd
rvzm <rvzm@protonmail.com>
2023-08-17 06:54:52 -0800

    added command to get users started with hunting

--- main.tcl
+++ main.tcl
@@ -80,6 +80,14 @@ namespace eval zboe {
 						return
 					}
 				}
+				if {$v2 == "zgo"} {
+					if {[catch {[zboe::procs::util::read_db "zhunt.$nick.xp"]} err]} { zboe::procs::util::write_db "zhunt.$nick.xp" "0"; }
+					if {[catch {[zboe::procs::util::read_db "zhunt.$nick.ammo"]} err]} { zboe::procs::util::write_db "zhunt.$nick.ammo" "6"; }
+					if {[catch {[zboe::procs::util::read_db "zhunt.$nick.clips"]} err]} { zboe::procs::util::write_db "zhunt.$nick.clips" "3"; }
+					if {[catch {[zboe::procs::util::read_db "zhunt.$nick.jam"]} err]} { zboe::procs::util::write_db "zhunt.$nick.jam" "no"; }
+					puthelp "PRIVMSG $chan :o.0.O.0.o. All set to hunt!";
+					return
+				}
 			}
 		}
 		proc zcontrol {nick uhost hand chan text} {
